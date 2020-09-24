59d223240d14007834977d9b8ffd352bbde4c10a
szabolcsmatyus <55321562+szabolcsmatyus@users.noreply.github.com>
2020-09-24 15:59:53 +0200

    Update

--- panel/database/seeds/NestSeeder.php
+++ panel/database/seeds/NestSeeder.php
@@ -45,7 +45,7 @@ class NestSeeder extends Seeder
         $this->createSourceEngineNest(array_get($items, 'Source Engine'));
         $this->createVoiceServersNest(array_get($items, 'Voice Servers'));
         $this->createRustNest(array_get($items, 'Rust'));
-        $this->createGTANest();
+        $this->createGTANest(array_get($items, 'GTA'));
     }
     /**
      * Create the Minecraft nest to be used later on.
