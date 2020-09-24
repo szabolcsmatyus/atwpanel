745ea7cabdcbc1fb9fd48c3c90b5ba453ae9b25d
szabolcsmatyus <55321562+szabolcsmatyus@users.noreply.github.com>
2020-09-24 12:40:12 +0200

    tarozó

---
+++ daemon.bat
@@ -0,0 +1,4 @@
+del daemon.tar.gz
+7z a -ttar daemon.tar daemon/
+7z a daemon.tar.gz daemon.tar
+del daemon.tar
---
+++ panel.bat
@@ -0,0 +1,4 @@
+del panel.tar.gz
+7z a -ttar panel.tar panel/
+7z a panel.tar.gz panel.tar
+del panel.tar
