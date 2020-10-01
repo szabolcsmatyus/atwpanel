bbb436548db7af0b0503ba7f36d24cc1135e2e69
szabolcsmatyus <55321562+szabolcsmatyus@users.noreply.github.com>
2020-10-01 12:05:24 +0200

    a

--- panel/resources/themes/pterodactyl/layouts/auth.blade.php
+++ panel/resources/themes/pterodactyl/layouts/auth.blade.php
@@ -40,17 +40,21 @@
                 <button id="order-close-btn">Vissza</button>
             </div>
             <div class="row justify-content-md-center">
-                <div class="col-lg-8 gamelogos">
-                    <img class="gamelogo" src="/assets/png/csgo.png" id="csgo">
-                    <img class="gamelogo" src="/assets/png/gtav.png" id="gtav">
-                    <img class="gamelogo" src="/assets/png/rust.png" id="rust">
-                    <img class="gamelogo" src="/assets/png/minecraft.png" id="minecraft">
-                    <img class="gamelogo" src="/assets/png/ark.png" id="ark">
-                    <img class="gamelogo" src="/assets/png/conanexiles.png" id="conanexiles">
-                </div>
-                <div class="col col-lg-4 order-form">
-                    rendelés form
-                </div>
+                <form>
+                    <div class="col-lg-8 gamelogos">
+                        <select>
+                            <input type="checkbox" class="gamelogo" style="background-image:url(/assets/png/csgo.png)" id="csgo">
+                            <input type="checkbox" class="gamelogo" style="background-image:url(/assets/png/gtav.png)" id="gtav">
+                            <input type="checkbox" class="gamelogo" style="background-image:url(/assets/png/rust.png)" id="rust">
+                            <input type="checkbox" class="gamelogo" style="background-image:url(/assets/png/minecraft.png)" id="minecraft">
+                            <input type="checkbox" class="gamelogo" style="background-image:url(/assets/png/ark.png)" id="ark">
+                            <input type="checkbox" class="gamelogo" style="background-image:url(/assets/png/conanexiles.png)" id="conanexiles">
+                        </select>
+                    </div>
+                    <div class="col col-lg-4 order-form">
+                        rendelés form
+                    </div>
+                </form>
             </div>
         </div>
         <div class="container">
