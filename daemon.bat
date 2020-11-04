91b30404ca21b214f5715c18d4fd51d14540d1b9
szabolcsmatyus <55321562+szabolcsmatyus@users.noreply.github.com>
2020-11-04 16:54:17 +0100

    a

--- daemon.bat
+++
@@ -1,4 +0,0 @@
-del daemon.tar.gz
-7z a -ttar daemon.tar daemon/
-7z a daemon.tar.gz daemon.tar
-del daemon.tar
--- panel.bat
+++
@@ -1,4 +0,0 @@
-del panel.tar.gz
-7z a -ttar panel.tar panel/
-7z a panel.tar.gz panel.tar
-del panel.tar
